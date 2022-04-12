42e51fd0033beab9dbf0eacfaf369e0c9bdccac7
Fradiano <103416758+Fradiano@users.noreply.github.com>
2022-04-12 14:47:40 +0200

    Update Main.js
    Hinzugefügt, das wars, dies das.

--- Main.js
+++ Main.js
@@ -2,3 +2,9 @@ function helloWorld() {
 // This line prints a simple message console.log("Hello World");
     // Return value
     return 0; }
+unction main() {
+console.log("Calling the function: ");
+   // Function call
+   helloWorld();
+   // Return value
+return 0; }
